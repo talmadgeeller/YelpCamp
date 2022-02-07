e46dd0ac483411f4f26714b18c2da4a3a31ed781
talmadgeeller <66578233+talmadgeeller@users.noreply.github.com>
2022-02-06 21:55:47 -0500

    Added bootstrap styling to all pages

--- seeds/index.js
+++ seeds/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const colors = require('../styles/styles')
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
-const { dbUser, dbPass, dbName, hostname } = require('./auth/auth')
+const { dbUser, dbPass, dbName, hostname } = require('../auth/auth')
 const Campground = require('../models/campground');
 const onlineDatabase = true;
 
@@ -30,9 +30,20 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing' +
+                'elit. Iure quidem, dignissimos voluptatibus a id officia. ' +
+                'Perspiciatis, adipisci corporis. Omnis, quae nihil dolorem ' +
+                'maxime eaque praesentium natus incidunt accusamus aspernatur ' +
+                'eius! Dignissimos voluptate alias illo asperiores quisquam ' +
+                'dolorum aliquid exercitationem, magnam aliquam modi vel, dicta ' +
+                'fuga ipsum assumenda voluptatibus qui hic doloribus. Illo ' +
+                'laboriosam quae odit nam commodi cupiditate error cumque.',
+            price
         })
         await camp.save();
     }
