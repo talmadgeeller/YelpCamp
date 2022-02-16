ea17e9779dab9d23a4e64c8f999a8bde7cdcaab6
talmadgeeller <66578233+talmadgeeller@users.noreply.github.com>
2022-02-16 11:33:18 -0500

    Changed basic styling of homepage

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,10 +1,12 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map',
+    container: 'cluster-map',
     style: 'mapbox://styles/mapbox/dark-v10',
     center: [-98.2, 38.2],
     zoom: 3.5
 });
+
+map.addControl(new mapboxgl.NavigationControl());
 
 map.on('load', () => {
     // Add a new source from our GeoJSON data and
