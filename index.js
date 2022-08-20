c4c70c4f9d2af0776babda2f2d83568f446fba80
talmadgeeller <66578233+talmadgeeller@users.noreply.github.com>
2022-08-20 08:38:03 -0400

    Changed database connected method

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ mongoose.connect(DB_URL);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection Error:"));
 db.once("open", () => {
-    console.log(colors['green'], "Database Connected");
+    console.log(colors['purple'], `Database '${DB_NAME}' Connected`);
 });
 
 // Launches express application
